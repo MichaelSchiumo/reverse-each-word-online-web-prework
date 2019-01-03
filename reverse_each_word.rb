845d305a3f4a1d14ab4e43e522f17fa6f4289cda
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-03 19:54:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,13 @@
 def reverse_each_word(string)
  string = string.split()
  reversed_string = []
-  string.each do |char|
+  reversed_string.collect {|x| x
     reversed_string << char.reverse! 
     end   
   return reversed_string.join(' ')
 end    
 
-
+a = [ "a", "b", "c", "d" ]
+a.collect {|x| x + "!" }   #=> ["a!", "b!", "c!", "d!"]
+a                          #=> ["a", "b", "c", "d"]
 
