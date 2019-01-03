215563fac6eb8b09a8cb64ac70c69c91d4104098
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-03 19:42:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-#def reverse_each_word(string)
- #string = string.split()
- #reversed_string = []
-  #string.each do |char|
-    #reversed_string << char.reverse! 
-    #end   
-  #return reversed_string.join(' ')
-#end    
+
 
 
 
