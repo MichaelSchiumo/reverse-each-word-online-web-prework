0436f188ed550ab3f33f4522b47e564ff3ebe220
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-03 19:35:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
-def reverse_each_word(string)
- string = string.split()
- reversed_string = []
-  string.each do |char|
-    reversed_string << char.reverse! 
-    end   
-  return reversed_string.join(' ')
-end    
+#def reverse_each_word(string)
+ #string = string.split()
+ #reversed_string = []
+  #string.each do |char|
+    #reversed_string << char.reverse! 
+    #end   
+  #return reversed_string.join(' ')
+#end    
 
+
+def reverse_each_word(string)
 
