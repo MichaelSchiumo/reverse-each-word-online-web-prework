4331d23f2663fc0be94233e86d6019c6ce29cfa0
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-03 19:55:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
  string = string.split()
  reversed_string = []
-  reversed_string.collect {|x| x.reverse! }
+  reversed_string.collect {|x| x }
     reversed_string << x
     end   
   return reversed_string
